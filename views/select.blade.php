a328216323de15867c0f313a4c60c6793ca2241b
Hannes Vermeire <hannesvermeire@gmail.com>
2020-01-17 08:57:36 +0100

    Fix multiple select

--- views/select.blade.php
+++ views/select.blade.php
@@ -1,19 +1,29 @@
 <?php /** @var \Galahad\Aire\Elements\Attributes\Attributes $attributes */ ?>
 <?php /** @var \Galahad\Aire\Support\OptionsCollection $options */ ?>
+@php
+if ($attributes->get('multiple')) {
+$attributes->wrapper->get('class')->add('is-multiple');
+}
+@endphp
 <div {{ $attributes->wrapper }}>
     <select {{ $attributes->except('value') }}>
-
         @isset($prepend_empty_option)
-            <option value="{{ $prepend_empty_option->value }}" {{ $attributes->isValue($prepend_empty_option->value) ? 'selected' : '' }}>
-                {{ $prepend_empty_option->label }}
-            </option>
+        <option
+            value="{{ $prepend_empty_option->value }}"
+            {{ $attributes->isValue($prepend_empty_option->value) ? 'selected' : '' }}
+        >
+            {{ $prepend_empty_option->label }}
+        </option>
         @endisset
 
         @foreach($options->getOptions() as $value => $label)
 
-            <option value="{{ $value }}" {{ $attributes->isValue($value) ? 'selected' : '' }}>
-                {{ $label }}
-            </option>
+        <option
+            value="{{ $value }}"
+            {{ $attributes->isValue($value) ? 'selected' : '' }}
+        >
+            {{ $label }}
+        </option>
 
         @endforeach
     </select>
